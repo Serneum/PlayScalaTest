014ff2c8e4c0ad967305eb619f32489e9b027597
crr <crr@us.ibm.com>
2014-11-14 11:35:54 -0500

    Experimented with splitting REST services into separate controllers
    Change-Id: Id0096f048e919694d0133d0ecfcfea35f539d966

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -20,36 +20,4 @@ object Application extends Controller {
     Ok(views.html.workers(Workers.list))
   }
 
-  val fileList = Action { implicit request =>
-    Ok(views.html.fileList(Files.list))
-  }
-
-  val upload = Action(parse.multipartFormData) { request =>
-    request.body.file("file").map { file =>
-      val fileName = file.filename
-      val filePath = "files/" + fileName
-      val uploadedFile = new java.io.File(filePath);
-      uploadedFile.getParentFile().mkdirs()
-      file.ref.moveTo(uploadedFile)
-      Files.add(new scratch.File(fileName, filePath))
-      Redirect(routes.Application.fileList)
-    }
-    .getOrElse {
-      Redirect(routes.Application.fileList).flashing("error" -> "Missing file")
-    }
-  }
-
-  def deleteFile(name: String) = Action(parse.anyContent) { request =>
-    val filePath = "files/" + name
-    val file = new java.io.File(filePath)
-    if (file.exists()) {
-      if (file.delete()) {
-        Files.remove(name)
-        Redirect(routes.Application.fileList)
-      }
-      Redirect(routes.Application.fileList).flashing("error" -> s"Could not delete '$name'")
-    }
-    Redirect(routes.Application.fileList).flashing("error" -> s"'$name' does not exist.")
-  }
-
 }
